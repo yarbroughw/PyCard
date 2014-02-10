fa8e18b707db82af3db1f5b439b854bbf4aca681
Willem Yarbrough <yarbroughw@allegheny.edu>
2014-02-10 13:15:35 -0500

    added time metric

--- pycard.py
+++ pycard.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
-import .path as path, os
+import os.path as path, os
 import argparse
+import time
 
 def _makeparser():
   parser = argparse.ArgumentParser()
   parser.add_argument("rootdir", help="specifies root directory for traversal")
   parser.add_argument("-m","--maxfiles", help="max number of files to traverse over",
-      type=int)
+                        type=int)
   return parser
 
 class enterprise:
@@ -18,7 +19,6 @@ class enterprise:
   data = { 
             'totalnum':   0, 
             'totalsize':  0,
-            'maxbreadth': 0, 
             'maxdepth':   0, 
             'average':    0
          }
@@ -26,10 +26,10 @@ class enterprise:
   def logfile(self,item):
     """logs files encountered during trek()"""
     self.data['totalnum'] += 1
-    self.log[item] = os.stat(item)  # create a log entry about "item"
+    self.log[item] = os.stat(item)                    # create a log entry about "item"
     
   def analyze(self):
-    """analyzes file data in log"""
+    """returns metadata taken from log"""
     totalsize = 0
     log = self.log
     data = self.data
@@ -39,15 +39,24 @@ class enterprise:
       return
 
     for item in log:
-      data['totalsize'] += log[item].st_size
+      data['totalsize'] += log[item].st_size          # update total size count
 
-      currentdepth = item.count(os.sep)               # count '/' chars in filepath
+      currentdepth = item.count(os.sep)               # count '/' chars in filepath to measure depth
       if currentdepth > data['maxdepth']: 
-        data['maxdepth'] = currentdepth
+        data['maxdepth'] = currentdepth               # update max depth if biggest
 
     data['average'] = data['totalsize'] / data['totalnum']
     self.data = data
     return
+
+  def printdata(self):
+    
+    print "   total file count \t=  {}".format(self.data['totalnum'])
+    print "   total size \t\t=  {} bytes ({:.2f} MB)".format(self.data['totalsize'], 
+                                                    self.data['totalsize']/float(1048576))
+    print "   average size \t=  {} bytes ({:.2f} MB)".format(self.data['average'], 
+                                                    self.data['average']/float(1048576))
+    print "   max depth \t\t=  {} directories".format(self.data['maxdepth'])
 
   def trek(self, directory, maxfiles):
     """recursively traverses the file system, starting at 'directory'"""
@@ -57,7 +66,7 @@ class enterprise:
         if not path.islink(itempath):                                 # dont follow symlinks
           if path.isdir(itempath):                                    # follow directories
             self.trek(itempath,maxfiles)
-          elif maxfiles == None or self.data['total'] < maxfiles:     # dont exceed max
+          elif maxfiles == None or self.data['totalnum'] < maxfiles:  # dont exceed max (from args)
             self.logfile(itempath)
           else:                                                       # otherwise, return
             return
@@ -72,17 +81,15 @@ def main():
 
   root = path.abspath(args.rootdir)
   print 'traversing file system with {} as root...'.format(root)
+  t1 = time.time()
   e.trek(root, args.maxfiles)
+  t2 = time.time()
+  print 'traversing the file system took %0.3f ms.' % ((t2-t1)*1000.0)
 
   print 'analyzing results...'
   e.analyze()
-
-  print 'traversal from {} shows:'.format(root)
-  print "   total file count \t=  {}".format(e.data['totalnum'])
-  print "   total size \t\t=  {} bytes".format(e.data['totalsize'])
-  print "   average size \t=  {} bytes".format(e.data['average'])
-  print "   max breadth \t\t=  {}".format(e.data['maxbreadth'])
-  print "   max depth \t\t=  {} directories".format(e.data['maxdepth'])
+  print "traversal from {} shows:".format(root)
+  e.printdata()
 
 # standard python boilerplate
 if __name__ == '__main__':
